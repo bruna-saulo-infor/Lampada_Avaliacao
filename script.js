207a688fb496487ccc686aef450ab5b8b1598ce9
Bruna e Saulo <brunabarbosaa0807@gmail.com>
2024-04-03 14:03:06 -0300

    Luz

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender() {
+    var lampada = document.getElementById ('aceso');
+    lampada.src = 'ligado.jpg';
+}
+
+function desligar() {
+    var lampada = document.getElementById ('aceso');
+    lampada.src = 'desligado.jpg';
+}
